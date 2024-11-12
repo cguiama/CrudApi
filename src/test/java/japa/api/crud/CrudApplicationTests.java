6ea89e10203131e5495544e6edf746dc820e4c23
Guilherme <45200933+cguiama@users.noreply.github.com>
2024-11-12 13:32:14 -0300

    crud

---
+++ src/test/java/japa/api/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package japa.api.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
